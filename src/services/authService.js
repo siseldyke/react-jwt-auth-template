06cd816d4c15b0175c0bb025e4106ed9a6bdfad2
siseldyke <siseldyke@gmail.com>
2024-11-13 15:04:11 -0500

    everything working except username showing

--- src/services/authService.js
+++ src/services/authService.js
@@ -9,18 +9,52 @@ const signup = async (formData) => {
         if (res.err) {
             throw new Error(res.err)
         };
-        return res.data;
+        if (res.data.token) {
+            localStorage.setItem('token', res.data.token)
+
+            const user = JSON.parse(atob(res.data.token.split('.')[1]))
+            return user
+        }
+        return res.data
     } catch (err) {
         console.log(err)
         throw err;
     }
 };
 
+const signin = async (user) => {
+    try {
+        const res = await axios.post(`${BACKEND_URL}/users/signin`, user)
+        if (res.data.err){
+            throw new Error(res.data.err)
+        }
+        if (res.data.token) {
+            localStorage.setItem('token', res.data.token)
 
+            const user = JSON.parse(atob(res.data.token.split('.')[1]))
+            return user
+        }
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
 
+const getUser =() =>{
+    const token = localStorage.getItem('token')
+    if(!token) return null
 
+    const user = JSON.parse(atob(token.split('.')[1]))
+    return user
+}
+
+const signout = () => {
+    localStorage.removeItem('token');
+  };
 
 export {
     signup,
-
+    signin,
+    getUser,
+    signout
 }
