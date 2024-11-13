6610c3e0d752e705bdeddea40b47f9e7c18a56a2
siseldyke <siseldyke@gmail.com>
2024-11-13 12:52:01 -0500

    navbar and such complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+
+
 
 const App = () => {
   const [user, setUser] = useState(null)
@@ -10,7 +14,13 @@ const App = () => {
   return (
     <>
       <NavBar user={user} />
-      <h1>Hello world!</h1>
+      <Routes>
+        { user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+      </Routes>
     </>
   )
 }
